5d92d49b47c798883670d2238b348446c78d30d6
dha04 <dha04@uw.edu>
2024-05-26 21:48:25 -0700

    another commit

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -34,7 +34,7 @@ export default function AuthProvider({ children }) {
         return () => {
             unsubscribed();
         };
-    }, []);
+    }, [navigate]);
 
     return (
         <AuthContext.Provider value={{ user }}>
